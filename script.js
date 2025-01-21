86daf71a4540282fb9510a4a2022f8523b22a587
Pester Mbhetse <105778274+Pestingo@users.noreply.github.com>
2025-01-21 14:34:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
+
+// Lazy load background images
+document.querySelectorAll('.swiper-slide').forEach(slide => {
+  const background = slide.getAttribute('data-background');
+  if (background) {
+    slide.style.backgroundImage = `linear-gradient(to top, #0f2027, #203a4300, #2c536400), url(${background})`;
+  }
+});
